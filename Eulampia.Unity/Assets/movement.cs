24e24347fb44b13ba547fd495bd3d200c6d042a1
Bals Eric <balseric7@gmail.com>
2018-06-15 15:29:22 +0300

    Add movement

---
+++ Eulampia.Unity/Assets/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.D))
+            rb.AddForce(10, 0, 0);
+        else
+                 if (Input.GetKeyDown(KeyCode.W))
+                    rb.AddForce(0, 0, 10);
+                 else
+                        if (Input.GetKeyDown(KeyCode.A))
+                            rb.AddForce(-10, 0, 0);
+                         else
+                                if (Input.GetKeyDown(KeyCode.S))
+                                     rb.AddForce(0, 0, -10);
+                             
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(0, 10, 0);
+
+    }
+}
