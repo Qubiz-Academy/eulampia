c025adfa73d68c8571b32056993f5c471f0c3cce
Bals Eric <balseric7@gmail.com>
2018-06-15 16:40:38 +0300

    1st

--- Eulampia.Unity/Assets/movement.cs
+++ Eulampia.Unity/Assets/movement.cs
@@ -1,30 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class movement : MonoBehaviour {
     public Rigidbody rb;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.D))
-            rb.AddForce(100, 0, 0);
-        else
-                 if (Input.GetKeyDown(KeyCode.W))
-                    rb.AddForce(0, 0, 100);
-                 else
-                        if (Input.GetKeyDown(KeyCode.A))
-                            rb.AddForce(-100, 0, 0);
-                         else
-                                if (Input.GetKeyDown(KeyCode.S))
-                                     rb.AddForce(0, 0, -100);
-                             
-        if (Input.GetKeyDown(KeyCode.Space))
-            rb.AddForce(0, 100, 0);
+    public Transform transform;
+    public bool IsGrounded { get; set; }
 
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "ground")
+        {
+           
+            IsGrounded = true;
+        }
     }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.D) && IsGrounded)
+            rb.AddForce(50, 0, 0);
+        else
+                 if (Input.GetKey(KeyCode.W) && IsGrounded)
+                    rb.AddForce(0, 0, 50);
+                 else
+                        if (Input.GetKey(KeyCode.A) && IsGrounded)
+                            rb.AddForce(-50, 0, 0);
+                         else
+                                if (Input.GetKey(KeyCode.S) && IsGrounded)
+                                     rb.AddForce(0, 0, -50);
+        if (transform.position.y > 0.5 && transform.position.y < 1)
+            if (IsGrounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(0, 8, 0, ForceMode.VelocityChange);
+                IsGrounded = false;
+            }
+
+        if (transform.position.y < -1)
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            transform.position = new Vector3(0.22f, 0.9076411f, -1.84f);
+        }
 }
