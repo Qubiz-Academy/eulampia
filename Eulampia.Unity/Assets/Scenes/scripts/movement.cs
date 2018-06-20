c6f5ad4adf233d016248bcf5d69784e1f1d3af91
Bals Eric <balseric7@gmail.com>
2018-06-20 16:45:54 +0300

    sfsaf

--- Eulampia.Unity/Assets/Scenes/scripts/movement.cs
+++ Eulampia.Unity/Assets/Scenes/scripts/movement.cs
@@ -1,27 +1,42 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
-public class movement : checkpoint {
+public class movement : checkpoint
+{
     public Rigidbody rb;
     public Transform transform;
     public bool IsGrounded { get; set; }
     public bool IsPause { get; set; }
     public bool SP { get; set; }
+    public bool SP2 { get; set; }
+    public bool IsJumping { get; set; }
     Rigidbody RigidBodyP;
     private void OnCollisionEnter(Collision collisionInfo)
     {
+        IsJumping = false;
         if (collisionInfo.collider.tag == "ground")
         {
 
             IsGrounded = true;
+
         }
-        if (collisionInfo.collider.tag == "specialPlatform")
+        else if (collisionInfo.collider.tag == "specialPlatform")
         {
             SP = true;
             IsGrounded = true;
         }
         else
+        {
+            IsGrounded = false;
             SP = false;
+        }
+        if (collisionInfo.collider.tag == "specialplatfom2")
+        {
+            SP2 = true;
+            IsGrounded = true;
+        }
+        else
+            SP2 = false;
 
         if (collisionInfo.collider.tag == "Obs")
         {
@@ -30,13 +45,13 @@ public class movement : checkpoint {
 
 
                 transform.position = new Vector3(0.22f, 0.9076411f, -1.84f);
-               
+
             }
             else
-                 {
+            {
                 transform.position = new Vector3(86.2f, 0.95f, -1.84f);
-               
-                }
+
+            }
 
         }
         if (collisionInfo.collider.tag == "finish")
@@ -47,7 +62,7 @@ public class movement : checkpoint {
         }
     }
 
-    
+
     public GameObject Pause;
     public void pause()
     {
@@ -62,7 +77,7 @@ public class movement : checkpoint {
             RigidBodyP = GetComponent<Rigidbody>();
 
             RigidBodyP.constraints = RigidbodyConstraints.FreezeAll;
-           
+
             Pause.SetActive(true);
             IsPause = true;
         }
@@ -70,55 +85,55 @@ public class movement : checkpoint {
     public GameObject platform;
     public void platformPosition()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, platform.transform.position.z);
+        if (transform.position.z > platform.transform.position.z + 2.3f)
+            transform.position = new Vector3(transform.position.x, transform.position.y, platform.transform.position.z + 2.3f);
+        else if (transform.position.z < platform.transform.position.z - 2.3f)
+            transform.position = new Vector3(transform.position.x, transform.position.y, platform.transform.position.z - 2.3f);
     }
     public void Update()
     {
 
-       
+
 
         if (Input.GetKeyDown(KeyCode.Escape))
             pause();
 
-        if (SP == true && !Input.anyKey)
+        if (SP)
             platformPosition();
 
+        if (SP2)
+        {
+            RigidBodyP = GetComponent<Rigidbody>();
 
+            RigidBodyP.constraints = RigidbodyConstraints.FreezePositionZ;
+        }
+        else
+        {
+            RigidBodyP = GetComponent<Rigidbody>();
 
+            RigidBodyP.constraints = RigidbodyConstraints.None;
+        }
 
-        if (Input.GetKey(KeyCode.D) && IsGrounded)
-
+        if (Input.GetKey(KeyCode.D) && !IsJumping)
             rb.AddForce(40, 0, 0);
-
-
-        else
-                 if (Input.GetKey(KeyCode.W) && IsGrounded)
-
+        else if (Input.GetKey(KeyCode.W) && !IsJumping)
             rb.AddForce(0, 0, 40);
-
-
-        else
-                        if (Input.GetKey(KeyCode.A) && IsGrounded)
-
+        else if (Input.GetKey(KeyCode.A) && !IsJumping)
             rb.AddForce(-40, 0, 0);
-
-
-
-        else
-                                if (Input.GetKey(KeyCode.S) && IsGrounded)
-
+        else if (Input.GetKey(KeyCode.S) && !IsJumping)
             rb.AddForce(0, 0, -40);
 
-
-
-
         if (transform.position.y > 0.5)
-            if (IsGrounded && Input.GetKeyDown(KeyCode.Space))
+            if (IsGrounded && Input.GetKeyDown(KeyCode.Space) && !IsJumping)
             {
                 rb.AddForce(0, 400, 0);
+                IsJumping = true;
                 IsGrounded = false;
                 if (SP)
                     SP = false;
+                if (SP2)
+                    SP2 = false;
+
             }
 
         if (transform.position.y < -1)
