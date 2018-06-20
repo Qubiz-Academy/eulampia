683a8343a8e07d11a5178c50c2f5c78a5fb3c9aa
Bals Eric <balseric7@gmail.com>
2018-06-20 16:58:39 +0300

    part

---
+++ Eulampia.Unity/Assets/explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour {
+
+    public ParticleSystem part;
+    movement mv;
+    
+	void Start () {
+        part.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mv.transform.position.x < 35 && mv.transform.position.x > 30)
+            part.Play();
+	}
+}
