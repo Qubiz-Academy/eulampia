5d11076aef0acd068e9ac4bc6845e8c249e9b42d
Bals Eric <balseric7@gmail.com>
2018-06-22 15:09:41 +0300

    asdsa

---
+++ Eulampia.Unity/Assets/CheckpointArrival.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointArrival : MonoBehaviour {
+    public ParticleSystem part;
+    public Movement ms; 
+	// Use this for initialization
+	void Start () {
+        ms = GameObject.Find("player").GetComponent<Movement>();
+
+        part.Stop();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Eulampia.Unity/Assets/Scenes/scripts/Checkpoint.cs
+++ Eulampia.Unity/Assets/Scenes/scripts/Checkpoint.cs
@@ -5,13 +5,18 @@ public class Checkpoint : MonoBehaviour
 {
     // Use this for initialization
     public int i = 0;
-
+    public CheckpointArrival Part;
+    private void Start()
+    {
+        Part = GameObject.Find("PlasmaExplosionEffect").GetComponent<CheckpointArrival>();
+    }
     public Vector3 checkpointS = new Vector3(0.22f, 0.9076411f, -1.84f);
     private void OnTriggerEnter(Collider other)
     {
         i++;
         if (i == 1)
         {
+            
             checkpointS = new Vector3(86.2f, 0.95f, -1.84f);
         }
     }
