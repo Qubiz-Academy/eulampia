849f940625ec0a3bb6afa916d0513dc1d8d4f743
Bals Eric <balseric7@gmail.com>
2018-06-23 17:54:19 +0300

    lvl2

---
+++ Eulampia.Unity/Assets/Scenes/scripts/Set.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set : MonoBehaviour {
+    public Animation Animation;
+    public Transform player;
+    public Animator Animator;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        Animator.GetComponent<Animation>();
+        Animation.Stop();
+      
+	}
+    public void Play()
+    {
+        Animation.Play();
+    
+    }
+    // Update is called once per frame
+    void Update () {
+        if (player.position.x > 17)
+            Play();
+	}
+}
